7ab63ad8d874d82e6161a35a6af3538b8ff64dbd
lannancy <lantianyu@wacai.com>
2016-05-24 21:44:33 +0800

    上传index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lan on 16/5/24.
+ */
+
+var express = require('express');
+var app = express();
+var graphqlHTTP = require('express-graphql');
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.use('/graphql', graphqlHTTP({ schema: MyGraphQLSchema, graphiql: true }));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
