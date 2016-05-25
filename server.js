6d011a7470615bb666bcc8184c0602f18bac557f
lannancy <lantianyu@wacai.com>
2016-05-25 19:05:07 +0800

    增加内存数据的返回

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lan on 16/5/25.
+ */
+
+var graphql = require ('graphql').graphql
+var express = require('express')
+var graphQLHTTP = require('express-graphql')
+var Schema = require('./schema')
+
+// This is just an internal test
+var query = 'query { todos { id, title, completed } }'
+graphql(Schema, query).then( function(result) {
+    console.log(JSON.stringify(result,null," "));
+
+});
+
+var app = express()
+    .use('/', graphQLHTTP({ schema: Schema, pretty: true }))
+    .listen(3000, function (err) {
+        console.log('GraphQL Server is now running on localhost:3000');
+    });
