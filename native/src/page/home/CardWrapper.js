f55f70f42c9cbebb2e01eb39d7e20f70627c7520
许度庆 <xu.duqing@gmail.com>
2016-05-31 13:13:49 +0800

    add card

--- native/src/page/home/CardWrapper.js
+++ native/src/page/home/CardWrapper.js
@@ -6,6 +6,7 @@ import React from 'react'
 import {
     View,
     Image,
+    StyleSheet
 } from 'react-native'
 
 import {Text} from 'TBText'
@@ -13,21 +14,63 @@ import {Text} from 'TBText'
 export default class CardWrapper extends React.Component{
 
     render(){
-        <View>
-            <Text>
-                文章标题1
-            </Text>
+        return(
+            <View>
+                <View style={styles.rowBox}>
+                    <View style={styles.titleBox}>
+                        <Text numberOfLines={2} style={styles.title}>
+                            文章标题1,文章标题1,文章标题1,文章标题1,文章标题1文章标题1文章标题1文章标题1文章标题1文章标题1
+                        </Text>
 
-            <Image source={require('')}/>
+                        <View style={styles.moreBox}>
+                            <Text>
+                                1024人阅读
+                            </Text>
 
-            <Text>
-                文章标题2
-            </Text>
+                            <Text>
+                                作者:大光
+                            </Text>
+                        </View>
+                    </View>
 
-            <Text>
-                文章描述2
-            </Text>
-        </View>
+                    <Image style={{width: 84,height: 70}} source={{uri:'http://img.alicdn.com/tps/TB1kaazMpXXXXacXFXXXXXXXXXX-900-500.jpg'}}/>
+                </View>
+                <View style={styles.split}/>
+            </View>
+        )
+
     }
 }
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize:15,
+        color:'#131313',
+        fontWeight:'bold'
+    },
+    moreBox:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+    rowBox:{
+        flex: 1,
+        flexDirection: 'row',
+        borderBottomWidth:1,
+        borderBottomColor:'#ececec',
+        paddingBottom:12,
+        marginRight:11,
+        marginLeft:11,
+        marginTop:11
+    },
+    titleBox:{
+        flex:1,
+        justifyContent:'space-between',
+        marginRight:15
+    },
+    split:{
+        flex:1,
+        height:7,
+        backgroundColor:'#ebebeb'
+    }
+});
 
