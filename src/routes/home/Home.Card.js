67607f7da7a1cefb1cdadb20142016a4f566b540
许度庆 <xu.duqing@gmail.com>
2016-06-14 18:05:29 +0800

    fix 点击跳转

--- src/routes/home/Home.Card.js
+++ src/routes/home/Home.Card.js
@@ -8,32 +8,34 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 
 function CardWrapper({book}){
-    const {title,createTime,imageUrl} = book;
+    const {title,createTime,imageUrl,id} = book;
     const date = new Date(createTime || 0);
     return(
         <div className={s.card}>
-            <div style={{flex:1,display:'flex',padding:11}}>
-                <div style={{flex:1,display:'flex',flexDirection:'column',justifyContent:'center',marginRight:8}}>
-                    <div style={{
-                    fontSize:15,
-                    color:'#131313',
-                    fontWeight:'bold'}}>
-                        {title}
-                    </div>
-                    <div style={{justifyContent:'space-between',flexDirection:'row',display:'flex',marginTop: 8}}>
-                        <div>
-                            {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}
+            <a href={`/detail?id=${id}`} >
+                <div style={{flex:1,display:'flex',padding:11}} >
+                    <div style={{flex:1,display:'flex',flexDirection:'column',justifyContent:'center',marginRight:8}}>
+                        <div style={{
+                        fontSize:15,
+                        color:'#131313',
+                        fontWeight:'bold'}}>
+                            {title}
                         </div>
-                        <div>
-                            作者:大光
+                        <div style={{justifyContent:'space-between',flexDirection:'row',display:'flex',marginTop: 8}}>
+                            <div>
+                                {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}
+                            </div>
+                            <div>
+                                作者:大光
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                {imageUrl?
-                    <img style={{width: 84,height: 70}} src={imageUrl}/>
-                    :null}
-            </div>
+                    {imageUrl?
+                        <img style={{width: 84,height: 70}} src={imageUrl}/>
+                        :null}
+                </div>
+            </a>
 
             <div style={{flex:1,height:7,backgroundColor:'#ebebeb',display:'flex'}}></div>
         </div>
