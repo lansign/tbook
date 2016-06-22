dd84330c9a1c51e89475caea294eba59b93c8c7b
lannancy <lantianyu@wacai.com>
2016-06-22 18:08:13 +0800

    修改detail页面的展示

--- src/routes/detail/Detail.js
+++ src/routes/detail/Detail.js
@@ -18,31 +18,21 @@ export default class Detail extends React.Component{
             <div>
                 <Header backUrl="/"/>
                 <div style={{maxWidth:1000,margin:"0 auto"}}>
-                    <div >
+                    <div>
                         {imageUrl?
                             <img style={{width:'100%',height:'auto'}} src={imageUrl}/>
                             :null}
 
-                        <div style={{fontSize:21,padding:11,display:'flex'}}>
-                            {title}
-                        </div>
+                        <div style={{flexDirection:'row',flex:1,paddingLeft:"20px",paddingRight:"20px",paddingTop:"20px",display:'flex'}}>
+                            <img style={{width:'50px',height:'50px', borderRadius:"25px", borderWidth:'1px'}} src={author && author.picture ? author.picture:"http://s1.wacdn.com/wis/6/20124bb62868e77b_195x195.png"}/>
 
-                        <div style={{flexDirection:'row',flex:1,padding:11,display:'flex'}}>
-                            <div style={{fontSize:12,color:'#989898',display:'flex'}}>
-                                作者:{author?author.displayName:"null"}
-                            </div>
-
-                            <div style={{marginLeft:20,fontSize:12,color:'#989898',display:'flex'}}>
-                                {date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}
-                            </div>
-
-                            <div style={{marginLeft:20,fontSize:12,color:'#989898',display:'flex'}}>
-                                阅读133万+
+                            <div style={{paddingLeft:"10px", fontSize:18,color:'#000000',display:'flex', 'align-items': 'center', 'justify-content': 'center'}}>
+                                {author?author.displayName:"null"}
                             </div>
                         </div>
                     </div>
 
-                    <dev dangerouslySetInnerHTML= {{ __html: md(content) }}/>
+                    <div style={{paddingLeft:"20px",paddingRight:"20px",paddingBottom:"20px"}} dangerouslySetInnerHTML= {{ __html: md(content) }}/>
                 </div>
             </div>
         )
